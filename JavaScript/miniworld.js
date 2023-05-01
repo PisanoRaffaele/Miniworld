edfc506dc2e0dbede0cbfc3c41c9780d09aed1b6
federikowsky <f.federico0106@gmail.com>
2023-05-01 20:45:58 +0200

    aggiunte carte a home

--- JavaScript/miniworld.js
+++ JavaScript/miniworld.js
@@ -1,30 +1,53 @@
 window.onload = function () {
     var isLoggedIn = false; // Definisci una variabile per indicare se l'utente è loggato o no. In questo esempio, l'utente non è loggato.
-    var elemList = document.getElementsByName("personal");
-    for (var i = 0; i < elemList.length; i++) {
-        var elem = elemList[i];
+    $('[name="personal"]').each(function() {
+        var elem = $(this);
         if (isLoggedIn) {
-            elem.href = "";
-            elem.innerHTML = "Profilo";
+            elem.attr('href', '');
+            elem.html('Profilo');
         }
         else {
-            elem.href = "";
-            elem.innerHTML = "Registrati/Login";
+            elem.attr('href', '');
+            elem.html('Registrati/Login');
         }
-    }
-
-    document.querySelector('#hamburger').addEventListener('click', function () {
-        document.querySelector('.animated-togglebutton').classList.toggle('open');
-        document.querySelector('#dropdown-menu').classList.toggle('show');
     });
 
-    window.addEventListener('resize', function() {
-    if (window.innerWidth > 991) {
-        // Attiva la funzione quando la larghezza della finestra > 991px
-        document.querySelector('.animated-togglebutton').classList.remove('open');
-        document.querySelector('#dropdown-menu').classList.remove('show');
-    }
+    $('#hamburger').on('click', function() {
+        $('.animated-togglebutton').toggleClass('open');
+        $('#dropdown-menu').toggleClass('show');
+    });    
+
+    $(window).on('resize', function() {
+        if ($(window).innerWidth() > 991) {
+            // Attiva la funzione quando la larghezza della finestra > 991px
+            $('.animated-togglebutton').removeClass('open');
+            $('#dropdown-menu').removeClass('show');
+        }
+    });
+
+    // fetch game elements from server with ajax
+    $.ajax({
+        url: "fetch.php",
+        dataType: "json",
+        success: function(data) {
+          var html = data.map(function(value) {
+            var X = Math.floor(Math.random() * 50); // genera un numero casuale compreso tra 0 e 100
+            var Y = Math.floor(Math.random() * 50); // genera un numero casuale compreso tra 0 e 100
+            return `
+              <div class="card" style="background-position: ${X}% ${Y}%;">
+                <img src="${value.image}" alt="${value.title}" class="card-img-top p-3">
+                <div class="card-body p-2">
+                  <h4 class="card-title p-2">${value.title}</h4>
+                  <p class="card-text">${value.description}</p>
+                </div>
+              </div>
+            `;
+          }).join("");
+          
+          $(".game").html(html);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          console.log(textStatus, errorThrown);
+        }
     });
 }
-
-
--- init.php
+++ init.php
@@ -14,6 +14,7 @@
     <title>miniworld</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <!-- CSS include -->
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/home.css">
