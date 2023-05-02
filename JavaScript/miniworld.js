7c283435f0d7794bf20583b11eaccc6fc35ea860
federikowsky <f.federico0106@gmail.com>
2023-05-02 15:40:35 +0200

    cambiato css game home

--- JavaScript/miniworld.js
+++ JavaScript/miniworld.js
@@ -26,33 +26,36 @@ window.onload = function () {
     });
 
     // fetch game elements from server with ajax
-    $.ajax({
-        url: "fetch.php",
-        dataType: "json",
-        success: function (data) {
-            var html = data.map(function (value) {
-                var X = Math.floor(Math.random() * 30); // genera un numero casuale compreso tra 0 e 100
-                var Y = Math.floor(Math.random() * 30); // genera un numero casuale compreso tra 0 e 100
-                return `
-                    <div class="card" style="background-position: ${X}% ${Y}%;">
-                        <div class="logo-container">
-                            <img src="${value.image}" alt="${value.title}">
-                        </div>
-                        <div class="title-container">
-                            <h4 class="card-title">${value.title}</h4>
-                        </div>
-                        <div class="description-container">
-                            <p class="card-text">${value.description}</p>
-                            <a class="play-button" href="${value.link}">Let's play</a>
-                        </div>
-                    </div>
-                `;
-            }).join("");
-
-            $(".game").html(html);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log(textStatus, errorThrown);
-        }
-    });
 }
+$.ajax({
+    url: "fetch.php",
+    dataType: "json",
+    success: function (data) {
+        var html = data.map(function (value) {
+            var X = Math.floor(Math.random() * 30); // genera un numero casuale compreso tra 0 e 100
+            var Y = Math.floor(Math.random() * 30); // genera un numero casuale compreso tra 0 e 100
+            return `
+                <div class="card" style="background-position: ${X}% ${Y}%;">
+                    <div class="logo-container">
+                        <img src="${value.image}" alt="${value.title}">
+                    </div>
+                    <div class="title-container">
+                        <h4 class="card-title">${value.title}</h4>
+                    </div>
+                    <div class="description-container">
+                        <p class="card-text">${value.description}</p>
+                        <a class="play-button" href="${value.link}">Let's play</a>
+                    </div>
+                </div>
+            `;
+        }).join("");
+
+        $(".game-list").html(html);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+        console.log(textStatus, errorThrown);
+    }
+});
+
+
+
