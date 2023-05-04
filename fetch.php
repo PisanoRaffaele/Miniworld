60920e2f38b17fa6bd73edfe629dc3a0c4355940
PisanoRaffaele <pisano.1959863@studenti.uniroma1.it>
2023-05-04 17:30:05 +0200

    gioco guess

--- fetch.php
+++ fetch.php
@@ -3,18 +3,16 @@
 $elementi = array(
     array('id' => 1, 'title' => 'Memory', 'description' => 'Metti alla prova la tua memoria', 'image' => 'assets/Logo.png', 'link' => 'memory.php'),
     array('id' => 2, 'title' => 'Dot', 'description' => 'Quanti click riesci a fare in 10 secondi?', 'image' => 'assets/Logo.png', 'link' => 'dot.php'),
-    array('id' => 3, 'title' => 'Simon', 'description' => '...', 'image' => 'assets/Logo.png', 'link' => 'simon.php'),
-    array('id' => 4, 'title' => 'Titolo Gioco 4', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'image' => 'assets/Logo.png', 'link' => ''),
+    array('id' => 3, 'title' => 'Simon', 'description' => 'Quanta memoria hai?', 'image' => 'assets/Logo.png', 'link' => 'simon.php'),
+    array('id' => 4, 'title' => 'Guess The Word', 'description' => 'Indovina la parola', 'image' => 'assets/Logo.png', 'link' => 'guess_the_word.php'),
     array('id' => 5, 'title' => 'Titolo Gioco 5', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'image' => 'assets/Logo.png', 'link' => ''),
     array('id' => 6, 'title' => 'Titolo Gioco 6', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'image' => 'assets/Logo.png', 'link' => ''),
     array('id' => 7, 'title' => 'Titolo Gioco 7', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'image' => 'assets/Logo.png', 'link' => ''),
-    array('id' => 8, 'title' => 'Titolo Gioco 8', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'image' => 'assets/Logo.png', 'link' => ''),
-    array('id' => 9, 'title' => 'Titolo Gioco 9', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'image' => 'assets/Logo.png', 'link' => ''),
-    array('id' => 10, 'title' => 'Titolo Gioco 10', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'image' => 'assets/Logo.png', 'link' => '')
+    array('id' => 8, 'title' => 'Titolo Gioco 8', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'image' => 'assets/Logo.png', 'link' => '')
 );
 
 // Istruzioni inutili
-$random = array_rand($elementi, 10); // selezioniamo 3 elementi casuali dall'array
+$random = array_rand($elementi, 8); // selezioniamo 3 elementi casuali dall'array
 
 $response = array(); // array vuoto che conterrà la risposta
 
