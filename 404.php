a6b26ca430f90b4e98c09e73b971e4637a6146f6
federikowsky <f.federico0106@gmail.com>
2023-05-05 17:50:06 +0200

    finita pagina 404

--- 404.php
+++ 404.php
@@ -1,12 +1,13 @@
-<div class="container body-404">
-    <section class="notFound">
-        <div class="img">
-            <img id='first-foto' src="https://assets.codepen.io/5647096/backToTheHomepage.png" alt="Back to the Homepage" />
-            <img src="https://assets.codepen.io/5647096/Delorean.png" alt="El Delorean, El Doc y Marti McFly" style="max-width: 700px;"/>
-        </div>
-        <div class="text">
-            <h1>404</h1>
-            <h2>PAGE NOT FOUND</h2>
-        </div>
-    </section>
+<div class="container-fluid body-404">
+	<section class="notFound">
+		<div class="img">
+			<img id="back-404" src="assets/backtohome.png" alt="Back to the Homepage" />
+			<img id="car-404" src="https://assets.codepen.io/5647096/Delorean.png" alt="El Delorean, El Doc y Marti McFly"/>
+		</div>
+		<div class="text">
+			<h1>404</h1>
+			<h2>PAGE NOT FOUND</h2>
+		</div>
+		<p class="comment-404">The page you're looking for isn't here</p>
+	</section>
 </div>
