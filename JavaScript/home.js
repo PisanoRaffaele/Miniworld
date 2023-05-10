9601afe253aa6074a14e740935394fe2a44c881d
federikowsky <f.federico0106@gmail.com>
2023-05-11 00:58:54 +0200

    sistemata foto carta home

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -8,7 +8,7 @@ $(function () {
                 var Y = Math.floor(Math.random() * 30); // genera un numero casuale compreso tra 0 e 100
                 return `
                 <div class="card" style="background-position: ${X}% ${Y}%;">
-                    <img src="https://assets.website-files.com/61c070585317d242d3a59789/61c070585317d2fdf3a5982a_Hover%20Particles.png" class="jumping-foto">
+                    <img src="assets/cardFoto.png" class="jumping-foto">
                     <div class="logo-container">
                         <img src="${value.image}" alt="${value.title}">
                     </div>
