a5f9aba445d8f9bee1649560f5230c07c902a9c9
federikowsky <f.federico0106@gmail.com>
2023-05-11 02:55:23 +0200

    aggiunto 2048

--- fetch.php
+++ fetch.php
@@ -5,22 +5,11 @@ $elementi = array(
     array('id' => 2, 'title' => 'Dot', 'description' => 'Quanti click riesci a fare in 10 secondi?', 'image' => 'assets/Logo.png', 'link' => '?p=dot'),
     array('id' => 3, 'title' => 'Simon', 'description' => 'Quanta memoria hai?', 'image' => 'assets/Logo.png', 'link' => '?p=simon'),
     array('id' => 4, 'title' => 'Guess The Word', 'description' => 'Indovina la parola', 'image' => 'assets/Logo.png', 'link' => '?p=guess_the_word'),
-    array('id' => 5, 'title' => 'Titolo Gioco 5', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'image' => 'assets/Logo.png', 'link' => ''),
+    array('id' => 5, 'title' => '2048', 'description' => 'Riesci a fare 2048?', 'image' => 'assets/Logo.png', 'link' => '?p=2048'),
     array('id' => 6, 'title' => 'Titolo Gioco 6', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'image' => 'assets/Logo.png', 'link' => ''),
     array('id' => 7, 'title' => 'Titolo Gioco 7', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'image' => 'assets/Logo.png', 'link' => ''),
     array('id' => 8, 'title' => 'Titolo Gioco 8', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'image' => 'assets/Logo.png', 'link' => '')
 );
-
-// Istruzioni inutili
-$random = array_rand($elementi, 8); // selezioniamo 3 elementi casuali dall'array
-
-$response = array(); // array vuoto che conterrà la risposta
-
-foreach ($random as $index) {
-    $response[] = $elementi[$index]; // aggiungiamo l'elemento casuale all'array di risposta
-}
-// fino qui
-
 
 header('Content-Type: application/json'); // specifica il tipo di risposta come JSON
 echo json_encode($elementi); // restituisce l'array di risposta come JSON
--- index.php
+++ index.php
@@ -10,41 +10,20 @@ include 'head.php';
         <?php
         include "header.php";
 
-        switch ($page) {
-            case 'home':
-                include 'home.php';
-                break;
-            case 'memory':
-                include 'memory.php';
-                break;
-            case 'dot':
-                include 'dot.php';
-                break;
-            case 'simon':
-                include 'simon.php';
-                break;
-            case 'guess_the_word':
-                include 'guess_the_word.php';
-                break;
-            case 'about':
-                include 'about.php';
-                break;
-            case 'login':
-                include 'login.php';
-                break;
-            case 'registration':
-                include 'registration.php';
-                break;
-            default:
-                include '404.php';
-                break;
-        }
+        $php_page = $page . '.php';
 
+        if (file_exists($php_page)) {
+            include $php_page;
+        } else {
+            include '404.php';
+        }
         ?>
     </main>
+
     <?php
     include 'footer.php';
     ?>
+    
 </body>
 
 </html>
