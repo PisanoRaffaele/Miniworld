858ba4f87d6cf9409c6c48a2d0ba2f4484f565eb
PisanoRaffaele <pisano.1959863@studenti.uniroma1.it>
2023-05-14 22:13:41 +0200

    classifiche

---
+++ JavaScript/classifica.js
@@ -0,0 +1,31 @@
+function get_classifica($id, $revers, $game) {
+	$.ajax({
+		type: "POST",
+		url: "handle_db.php",
+		data: { gioco: $game, funzione: "richiedi_classifica", order: $revers },
+		dataType: "json",
+		success: function (response) {
+			var html = '<h1 class="textSide">Classifica</h1>'
+			html += '<table><thead><tr><th>Posizione</th><th>Username</th><th>Punteggio</th></tr></thead><tbody>';
+			$.each(response, function (i, item) {
+				html += '<tr><td>' + (i + 1) + '</td><td>' + item.username + '</td><td>' + item.punteggio + '</td></tr>';
+			});
+			html += '</tbody></table>';
+			$($id).html(html);
+		},
+		error: function (xhr, status, error) {
+			alert("Errore: " + xhr.responseText);
+		}
+	});
+};
+
+get_classifica('#classifica1', 'reverse', 'MEMORY');
+
+get_classifica('#classifica2', 'notReverse', 'DOT');
+
+get_classifica('#classifica3', 'notReverse', 'SIMON');
+
+get_classifica('#classifica4', 'reverse', 'GTW');
+
+
+
--- classifica.php
+++ classifica.php
@@ -1,39 +1,42 @@
 <div class="top-bg" >
-    <div class="container" id="container" >
+    <div id="container" >
         <h1 class="textCenter">Classifiche</h1>
         <div class="grid">
-            <div class="row-12">
-                <h1 class="textCenter">Memory</h1>
-            </div>
-            <div class="container classifica d-flex justify-content-center">
-                <h1 class="textSide">Classifica</h1>
-            </div>
 
-
-
-            <div class="row-12">
-                <h1 class="textCenter">DOT</h1>
-            </div>
-            <div class="container classifica d-flex justify-content-center">
-                <h1 class="textSide">Classifica</h1>
+            <div>
+                <div>
+                    <h1 class="textCenter">Memory</h1>
+                </div>
+                <div class="container classifica d-flex justify-content-center" id="classifica1">
+                    <h1 class="textSide">Classifica</h1>
+                </div>
             </div>
 
-
-
-            <div class="row-12">
-                <h1 class="textCenter">Simon</h1>
+            <div>
+                <div>
+                    <h1 class="textCenter">DOT</h1>
+                </div>
+                <div class="container classifica d-flex justify-content-center" id="classifica2">
+                    <h1 class="textSide">Classifica</h1>
+                </div>
             </div>
-            <div class="container classifica d-flex justify-content-center">
-                <h1 class="textSide">Classifica</h1>
+
+            <div>
+                <div>
+                    <h1 class="textCenter">SIMON</h1>
+                </div>
+                <div class="container classifica d-flex justify-content-center" id="classifica3">
+                    <h1 class="textSide">Classifica</h1>
+                </div>
             </div>
 
-
-        
-            <div class="row-12">
-                <h1 class="textCenter">GTW</h1>
-            </div>
-            <div class="container classifica d-flex justify-content-center">
-                <h1 class="textSide">Classifica</h1>
+            <div>
+                <div>
+                    <h1 class="textCenter">GTW</h1>
+                </div>
+                <div class="container classifica d-flex justify-content-center" id="classifica4">
+                    <h1 class="textSide">Classifica</h1>
+                </div>
             </div>
         </div>
 
